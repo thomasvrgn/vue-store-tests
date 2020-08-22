3eb08c93c04879053aa2736c957bf3229e0fe788
NessMC <contact@nessmc.fr>
2020-08-22 11:52:36 +0200

    🎉 Vuex store tests project created.

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>
+    {{ inputContent }}
+  </h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapMutations } from 'vuex'
+
+export default Vue.extend({
+  data: () => ({
+    inputContent: ''
+  }),
+  mounted: function (): void {
+    console.log('test', this.$store)
+    this.inputContent = this.$store.state.input.inputContent
+  }
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container p-4">
+    <input type="text" name="" id="" v-model=inputContent class="bg-gray-200 px-4 py-2 text-sm focus:outline-none rounded" @keydown.enter=process>
+    <span class="block m-2 font-mono">
+      {{ inputContent }}
+    </span>
+    <nuxt-link to="about">About</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { mapMutations } from 'vuex'
+
+export default Vue.extend({
+  data: () => ({
+    inputContent: ''
+  }),
+  methods: {
+    process: function (): void {
+      this.$store.commit('input/changeInputContent', this.inputContent)
+      console.log(this.$store)
+    }
+  }
+})
+</script>
